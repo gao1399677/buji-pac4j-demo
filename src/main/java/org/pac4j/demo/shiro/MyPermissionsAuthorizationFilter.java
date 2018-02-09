b2033b11e9bb7fb72357535433b20a64df6b6d36
gao1399677 <gao.shengsheng@qq.com>
2018-02-09 11:16:36 +0800

    load url-roles and url-permissions dynamically

---
+++ src/main/java/org/pac4j/demo/shiro/MyPermissionsAuthorizationFilter.java
@@ -0,0 +1,32 @@
+package org.pac4j.demo.shiro;
+
+import org.apache.shiro.web.filter.authz.PermissionsAuthorizationFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * <p>Title: MyPermissionsAuthorizationFilter </p>
+ * <p>Description: MyPermissionsAuthorizationFilter </p>
+ * Date: 2018年02月09日 下午11:03 AM
+ *
+ * @author gaosheng@hiynn.com
+ * @version 1.0 </p>
+ * Significant Modify：
+ * Date               Author           Content
+ * ==========================================================
+ * 2018年02月09日       SGao        创建文件,实现基本功能
+ * <p>
+ * ==========================================================
+ */
+public class MyPermissionsAuthorizationFilter extends PermissionsAuthorizationFilter {
+
+    @Override
+    public boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws IOException {
+        // use your services to load permissions
+        String[] perms = (String[]) mappedValue;
+
+        return super.isAccessAllowed(request, response, perms);
+    }
+}
---
+++ src/main/java/org/pac4j/demo/shiro/MyRolesAuthorizationFilter.java
@@ -0,0 +1,33 @@
+package org.pac4j.demo.shiro;
+
+import org.apache.shiro.web.filter.authz.RolesAuthorizationFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * <p>Title: My </p>
+ * <p>Description: My </p>
+ * Date: 2018年02月09日 下午11:10 AM
+ *
+ * @author gaosheng@hiynn.com
+ * @version 1.0 </p>
+ * Significant Modify：
+ * Date               Author           Content
+ * ==========================================================
+ * 2018年02月09日       SGao        创建文件,实现基本功能
+ * <p>
+ * ==========================================================
+ */
+public class MyRolesAuthorizationFilter extends RolesAuthorizationFilter {
+
+    @Override
+    public boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws IOException {
+
+        //use your services to load roles from database dynamically
+        String[] rolesArray = (String[]) mappedValue;
+
+        return super.isAccessAllowed(request, response, rolesArray);
+    }
+}
